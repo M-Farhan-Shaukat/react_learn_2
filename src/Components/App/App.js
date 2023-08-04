05acd772a54423e3388366aadcf1cb080f1c146f
farhan_shaukat <farhan.dev@pk.see.biz>
2023-08-04 15:58:38 +0500

    fix : listing fetched

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,37 +1,41 @@
 import React from "react";
 import "./App.css";
+import ContactListing from "../Customer/Listing"
 import ListItem from "../List/ListItem";
 import Instruction from "../Instructions/instruction";
 
 function App() {
-  const Emojis = [
-    {
-      name: "Grining Face",
-      emoji: "😀",
-    },
-    {
-      name: "Party Popper",
-      emoji: "🎉",
-    },
-    {
-      name: "Dancing Lady",
-      emoji: "💃",
-    },
-  ];
-  const displayParaProp = false;
   return (
-    <div className="container">
-      <h1>Hello World !</h1>
-      {displayParaProp && <p>Here i am Writing a paragraph </p>}
-      <Instruction />
-
-      <ul>
-        {Emojis.map((emoji) => (
-          <ListItem name={emoji.name} emoji={emoji.emoji} />
-        ))}
-      </ul>
-    </div>
+<ContactListing />
   );
+  // const Emojis = [
+  //   {
+  //     name: "Grining Face",
+  //     emoji: "😀",
+  //   },
+  //   {
+  //     name: "Party Popper",
+  //     emoji: "🎉",
+  //   },
+  //   {
+  //     name: "Dancing Lady",
+  //     emoji: "💃",
+  //   },
+  // ];
+  // const displayParaProp = false;
+  // return (
+  //   <div className="container">
+  //     <h1>Hello World !</h1>
+  //     {displayParaProp && <p>Here i am Writing a paragraph </p>}
+  //     <Instruction />
+
+  //     <ul>
+  //       {Emojis.map((emoji) => (
+  //         <ListItem name={emoji.name} emoji={emoji.emoji} />
+  //       ))}
+  //     </ul>
+  //   </div>
+  // );
 }
 
 export default App;
